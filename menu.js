d9fda336fdb9f682102d0c60224ab7106866f8ee
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-06-11 12:59:03 -0300

    atualizando o menu

--- menu.js
+++ menu.js
@@ -8,3 +8,14 @@ window.addEventListener('scroll', function() {
       menu.classList.remove('menu-rolado');
     }
   });
+
+  window.addEventListener('scroll', function() {
+    var imagem = document.getElementById('logo-agcont');
+    var posicao = window.scrollY;
+
+    if (posicao > 20) { // Altere o valor 500 para a posição em que deseja mudar a imagem
+      imagem.src = 'imagem2.jpg'; // Altere 'imagem2.jpg' para o caminho da nova imagem
+    } else {
+      imagem.src = 'imagem1.jpg'; // Altere 'imagem1.jpg' para o caminho da imagem original
+    }
+  });
