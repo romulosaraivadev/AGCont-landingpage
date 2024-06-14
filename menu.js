394aab833527f934c4296027caeb36b5e96da980
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-06-14 15:36:58 -0300

    çkjçj

--- menu.js
+++ menu.js
@@ -1,11 +1,14 @@
 window.addEventListener('scroll', function() {
     var menu = document.querySelector('#menu');
+    var menuMobile = document.getElementById ('navlist-menu')
     var scrollPosition = window.scrollY;
 
-    if (scrollPosition > 20 && window.innerWidth > 768) {
+    if (scrollPosition > 20/* && window.innerWidth > 768*/) {
       menu.classList.add('menu-rolado');
+      
     } else {
       menu.classList.remove('menu-rolado');
+      m
     }
   });
 
@@ -13,9 +16,16 @@ window.addEventListener('scroll', function() {
     var imagem = document.getElementById('logo-agcont');
     var posicao = window.scrollY;
 
-    if (posicao > 20 && window.innerWidth > 768) { 
+    if (posicao > 20 /*&& window.innerWidth > 768*/) { 
       imagem.src = 'img/logo-agcont-preta.svg'; 
     } else {
       imagem.src = 'img/logo-agcont.svg'; 
     }
   });
+
+  const hamburger = document.querySelector(".menu-mobile")
+  const nav = document.querySelector('.nav-menu')
+  const link = document.querySelector('link-menu')
+
+  hamburger.addEventListener("click", () => nav.classList.toggle("active"))
+  
